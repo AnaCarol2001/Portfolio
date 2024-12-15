dd72b45093d2e40c4b7d372adde20a98bd7d2174
AnaCarol <62663206+AnaCarol2001@users.noreply.github.com>
2024-12-14 22:59:17 -0300

    feat: adds more tag variants styles

--- src/components/ui/Tag/index.tsx
+++ src/components/ui/Tag/index.tsx
@@ -2,20 +2,32 @@ import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface TagProps extends ComponentProps<"span"> {
-  variant?: "main-purple" | "white";
+  variant?: "main-purple" | "white" | "white-outline";
+  size?: "sm" | "lg";
 }
 
 const TagVariants: Record<NonNullable<TagProps["variant"]>, string> = {
   "main-purple": "bg-main-purple text-white ",
   white: "bg-white text-main-purple ",
+  "white-outline": "bg-transparent border border-2 border-white",
 };
 
-export default function Tag({ variant = "main-purple", ...props }: TagProps) {
+const TagSize: Record<NonNullable<TagProps["size"]>, string> = {
+  sm: "rounded-lg px-2 py-1 text-sm font-bold",
+  lg: "text-lg py-1 px-4 md:px-8 rounded-full",
+};
+
+export default function Tag({
+  variant = "main-purple",
+  size = "sm",
+  ...props
+}: TagProps) {
   return (
     <span
       className={twMerge(
-        "inline-block text-nowrap rounded-lg px-2 py-1 text-sm font-bold",
+        "text- inline-block text-nowrap",
         TagVariants[variant],
+        TagSize[size],
       )}
       {...props}
     />
